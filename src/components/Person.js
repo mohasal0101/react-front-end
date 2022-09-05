107f3cd4f176805e7b3ee0b4bbc94c40139258a2
mohasal0101 <mohasal0101@gmail.com>
2022-09-05 18:26:19 +0300

    route

---
+++ src/components/Person.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+
+function Person () {
+    const [ name, setName ] = useState( "salah" );
+    const [ age, setAge ] = useState( 25 );
+    const [ gender, setGender ] = useState( "male" );
+
+    function handleSubmit ( event ) {
+        event.preventDefault();
+        setName( event.target.newName.value );
+        setGender( event.target.gender.value );
+        setAge( event.target.age.value );
+        axios.post( `${process.env.REACT_APP_SERVER}/person?name=${name}&age=${age}&gender=${gender}` )
+            .then( response => {
+                setAge( response.data );
+            } )
+            .catch( error => {
+                console.log( error );
+            } );
+    }
+    return (
+        <>
+        <div>
+            <h2>Person</h2>
+            <p data-testid="name">My name is {name}</p>
+            <p data-testid="age">My age is {age}</p>
+            <p data-testid="gender">My gender is {gender}</p>
+        </div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name="newName" data-testid="name-input" />
+                </label>
+                <br />
+                <label>
+                    Age:
+                    <input type="number" name="age" data-testid="age-input" />
+                </label>
+                <br />
+                <label>
+                    Gender:
+                    <select name="gender" data-testid="gender-input" >
+                        <option value="select">Select</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                </label>
+                <br />
+                <input type="submit" value="Submit" data-testid="submit"/>
+            </form>
+        </div>
+        </>
+    );
+}
+
+
+export default Person;
